3d8e029125b4751a90c68ca0a3a6aba7265cf0fe
Lê Quốc Nhâm <lqnham>
2020-12-10 00:45:44 +0700

    BE-4 refactor code

--- src/main/java/vn/prostylee/auth/service/impl/AuthServiceImpl.java
+++ src/main/java/vn/prostylee/auth/service/impl/AuthServiceImpl.java
@@ -1,5 +1,6 @@
 package vn.prostylee.auth.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
