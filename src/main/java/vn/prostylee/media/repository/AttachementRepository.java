2b458b648508aac523f321bbac170e858993d1d9
Phan Thanh Giang <ptgiang56it@gmail.com>
2020-12-21 01:04:53 +0700

    Migrate config MySQL to PostgreSql

--- src/main/java/vn/prostylee/media/repository/AttachementRepository.java
+++ src/main/java/vn/prostylee/media/repository/AttachementRepository.java
@@ -2,16 +2,16 @@ package vn.prostylee.media.repository;
 // Generated Nov 28, 2020, 9:47:00 PM by Hibernate Tools 5.2.12.Final
 
 import vn.prostylee.core.repository.BaseRepository;
-import vn.prostylee.media.entity.Attachement;
+import vn.prostylee.media.entity.Attachment;
 
 import org.springframework.stereotype.Repository;
 
 /**
  * Repository for domain model class Attachement.
- * @see Attachement;
+ * @see Attachment ;
  * @author prostylee
  */
 @Repository
-public interface AttachementRepository extends BaseRepository<Attachement, Long> {
+public interface AttachementRepository extends BaseRepository<Attachment, Long> {
 
 }
