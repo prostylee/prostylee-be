3f216378791b9903d3d33f552fbf7c80982b0827
Lê Quốc Nhâm <lqnham>
2020-12-20 23:01:26 +0700

    BE-78 REDUNDANT CODE

--- src/main/java/vn/prostylee/comment/controller/CommentController.java
+++ src/main/java/vn/prostylee/comment/controller/CommentController.java
@@ -2,16 +2,13 @@ package vn.prostylee.comment.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import vn.prostylee.auth.service.UserService;
 import vn.prostylee.comment.dto.filter.CommentFilter;
 import vn.prostylee.comment.dto.request.CommentRequest;
 import vn.prostylee.comment.dto.response.CommentResponse;
-import vn.prostylee.comment.entity.Comment;
 import vn.prostylee.comment.service.CommentService;
 import vn.prostylee.core.constant.ApiVersion;
 import vn.prostylee.core.controller.CrudController;
