d661b81757b0a7126d59bba3bfdbe56f72a7cdd8
Phan Thanh Giang <ptgiang56it@gmail.com>
2020-12-21 21:43:55 +0700

    Migrate config MySQL to PostgreSql

--- src/integrationTest/resources/db/migration/V1_0_1__Alter_User.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "user" ADD COLUMN dob_date int null;
-ALTER TABLE "user" ADD COLUMN dob_month int null;
-ALTER TABLE "user" ADD COLUMN dob_year int null;
-ALTER TABLE "user" ADD COLUMN avatar varchar(512) null;
