098df0ecb3df39d7736eaab120d82fd1cd2ad433
Lê Quốc Nhâm <lqnham>
2021-01-03 23:59:21 +0700

    BE-101 write some API such as count, average, get by filter and so on

--- src/main/java/vn/prostylee/useractivity/dto/request/UserRatingRequest.java
+++ src/main/java/vn/prostylee/useractivity/dto/request/UserRatingRequest.java
@@ -1,10 +1,17 @@
 package vn.prostylee.useractivity.dto.request;
 
+import com.google.firebase.database.annotations.NotNull;
 import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class UserRatingRequest {
+    @NotNull
     private Long targetId;
+
+    @NotBlank
     private String targetType;
+
     private Integer value;
 }
