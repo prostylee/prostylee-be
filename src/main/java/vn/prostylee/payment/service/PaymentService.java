e7addfb184bb152c83b3a4e72381d17d62b2169c
Lê Quốc Nhâm <lqnham>
2021-01-12 00:33:53 +0700

    BE-87 Handle Payment with master data

--- src/main/java/vn/prostylee/payment/service/PaymentService.java
+++ src/main/java/vn/prostylee/payment/service/PaymentService.java
@@ -1,8 +1,7 @@
 package vn.prostylee.payment.service;
 
-import vn.prostylee.core.service.CrudService;
-import vn.prostylee.payment.dto.request.PaymentRequest;
+import vn.prostylee.core.service.MasterDataService;
 import vn.prostylee.payment.dto.response.PaymentResponse;
 
-public interface PaymentService extends CrudService<PaymentRequest, PaymentResponse, Long> {
+public interface PaymentService extends MasterDataService<PaymentResponse> {
 }
