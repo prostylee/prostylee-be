8125258d19c8c2ef80d9bb4481843017e5e8c6b5
Lê Quốc Nhâm <lqnham>
2021-01-12 01:07:39 +0700

    BE-87 fix test

--- src/test/java/vn/prostylee/auth/controller/RoleControllerTest.java
+++ src/test/java/vn/prostylee/auth/controller/RoleControllerTest.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import vn.prostylee.auth.dto.response.RoleResponse;
 import vn.prostylee.auth.service.RoleService;
+import vn.prostylee.core.dto.filter.MasterDataFilter;
 
 import java.util.Collections;
 import java.util.List;
@@ -24,7 +27,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(MockitoExtension.class)
 class RoleControllerTest {
-
+    private static final int totalOfRecords = 1;
+    private static final int pageSize = 1;
     private MockMvc mockMvc;
 
     @Mock
@@ -38,15 +42,16 @@ class RoleControllerTest {
     @Test
     void getAllRoles_Successfully() throws Exception {
         List<RoleResponse> responses = Collections.singletonList(mockResponse());
-        when(roleService.findAll()).thenReturn(responses);
+        Page<RoleResponse> page = new PageImpl<>(responses);
+        when(roleService.findAll(new MasterDataFilter())).thenReturn(page);
 
         MvcResult mvcResult = this.mockMvc
                 .perform(get("/v1/roles"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("[0].id").value(1L))
-                .andExpect(jsonPath("[0].code").value("ADMIN"))
-                .andExpect(jsonPath("[0].name").value("Administrator"))
+                .andExpect(jsonPath("$.totalElements").value(totalOfRecords))
+                .andExpect(jsonPath("$.totalPages").value(totalOfRecords /pageSize))
+                .andExpect(jsonPath("$.content.length()").value(pageSize))
                 .andReturn();
         assertEquals(MediaType.APPLICATION_JSON_VALUE, mvcResult.getResponse().getContentType());
     }
