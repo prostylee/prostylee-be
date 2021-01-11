92eff65787f5ad8532dc8fca5d06b01ae97cccb7
Lê Quốc Nhâm <lqnham>
2021-01-11 20:24:45 +0700

    BE-87n Handle Payment API for get Only

--- src/main/java/vn/prostylee/payment/controller/PaymentController.java
+++ src/main/java/vn/prostylee/payment/controller/PaymentController.java
@@ -4,13 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vn.prostylee.core.constant.ApiVersion;
-import vn.prostylee.core.controller.MasterDataController;
+import vn.prostylee.core.controller.CrudController;
+import vn.prostylee.payment.dto.filter.PaymentFilter;
+import vn.prostylee.payment.dto.request.PaymentRequest;
 import vn.prostylee.payment.dto.response.PaymentResponse;
 import vn.prostylee.payment.service.PaymentService;
 
 @RestController
 @RequestMapping(value = ApiVersion.API_V1 + "/payments")
-public class PaymentController extends MasterDataController<PaymentResponse> {
+public class PaymentController extends CrudController<PaymentRequest, PaymentResponse, Long, PaymentFilter> {
 
     private PaymentService service;
 
