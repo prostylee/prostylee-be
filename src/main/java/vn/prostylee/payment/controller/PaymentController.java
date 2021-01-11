9bdca0b0c40a5e6d6a1762a59ede35220667cb7a
Lê Quốc Nhâm <lqnham>
2021-01-12 00:33:53 +0700

    BE-87 Handle Payment with master data

--- src/main/java/vn/prostylee/payment/controller/PaymentController.java
+++ src/main/java/vn/prostylee/payment/controller/PaymentController.java
@@ -4,15 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vn.prostylee.core.constant.ApiVersion;
-import vn.prostylee.core.controller.CrudController;
-import vn.prostylee.payment.dto.filter.PaymentFilter;
-import vn.prostylee.payment.dto.request.PaymentRequest;
+import vn.prostylee.core.controller.MasterDataController;
 import vn.prostylee.payment.dto.response.PaymentResponse;
 import vn.prostylee.payment.service.PaymentService;
 
 @RestController
 @RequestMapping(value = ApiVersion.API_V1 + "/payments")
-public class PaymentController extends CrudController<PaymentRequest, PaymentResponse, Long, PaymentFilter> {
+public class PaymentController extends MasterDataController<PaymentResponse> {
 
     private PaymentService service;
 
