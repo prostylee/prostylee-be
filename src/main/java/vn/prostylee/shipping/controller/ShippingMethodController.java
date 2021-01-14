d7d92368a303b5ddef00dfde3fdef372cf6b78fe
Lê Quốc Nhâm <lqnham>
2021-01-15 01:07:16 +0700

    BE-94 fix problem and separate the endpoint

--- src/main/java/vn/prostylee/shipping/controller/ShippingMethodController.java
+++ src/main/java/vn/prostylee/shipping/controller/ShippingMethodController.java
@@ -1,14 +1,17 @@
 package vn.prostylee.shipping.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vn.prostylee.core.constant.ApiVersion;
 import vn.prostylee.core.controller.MasterDataController;
+import vn.prostylee.core.dto.filter.MasterDataFilter;
 import vn.prostylee.shipping.dto.response.ShippingMethodResponse;
 import vn.prostylee.shipping.service.ShippingMethodService;
 
-@RestController("shippingMethodController")
+@RestController
 @RequestMapping(value = ApiVersion.API_V1 + "/shippings")
 public class ShippingMethodController extends MasterDataController<ShippingMethodResponse> {
     private ShippingMethodService service;
@@ -17,5 +20,10 @@ public class ShippingMethodController extends MasterDataController<ShippingMetho
     public ShippingMethodController(ShippingMethodService service) {
         super(service);
         this.service = service;
+    }
+
+    @GetMapping("/methods")
+    public Page<ShippingMethodResponse> getAll(MasterDataFilter filter){
+        return super.getAll(filter);
     }
 }
