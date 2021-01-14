3ad59902c09a17c57b75ef3545dea38988c32520
Lê Quốc Nhâm <lqnham>
2021-01-15 01:52:14 +0700

    BE-94 modify sql

--- src/main/resources/db/migration/V1_0_4__Alter_ShippingTypeShippingProvider.sql
+++ src/main/resources/db/migration/V1_0_4__Alter_ShippingTypeShippingProvider.sql
@@ -1,4 +1,6 @@
-ALTER TABLE shipping_type RENAME TO shipping_method;
+ALTER TABLE IF EXISTS shipping_type RENAME TO shipping_method;
+alter table shipping_method rename constraint shipping_type_pkey to shipping_method_pkey;
+alter index shipping_type_pkey rename to shipping_method_pkey;
 DROP TABLE IF EXISTS shipping_address;
 create table if not exists shipping_provider
 (
@@ -11,16 +13,17 @@ create table if not exists shipping_provider
     description varchar(512),
     name varchar(512),
     price double precision,
-    shipping_method_id bigint not null constraint "FK_shipping_method_shipping_provider" references shipping_type
+    shipping_method_id bigint not null constraint "FK_shipping_method_shipping_provider" references shipping_method
 );
 
 alter table shipping_provider owner to postgres;
+insert into shipping_method (id, created_at, description, name) values (1, '2020-12-11 16:09:49', 'Grab Express', 'GRAB');
 
-insert into shipping_provider (id, created_at, description, name, price) values (10010, '2020-12-11 16:09:49', 'Grab Express', 'GRAB', 25000);
-insert into shipping_provider (id, created_at, description, name, price) values (20010, '2020-12-11 16:10:49', 'VN Express', 'VNE', 30000);
-insert into shipping_provider (id, created_at, description, name, price) values (30010, '2020-12-11 16:11:49', 'Viettel Post', 'VTP', 25000);
-insert into shipping_provider (id, created_at, description, name, price) values (40010, '2020-12-11 16:12:49', 'Giao hang tiet kiem', 'GHTK', 25000);
-insert into shipping_provider (id, created_at, description, name, price) values (50010, '2020-12-11 16:13:49', 'Ninja van', 'NJV', 27000);
+insert into shipping_provider (id, created_at, description, name, price, shipping_method_id) values (10010, '2020-12-11 16:09:49', 'Grab Express', 'GRAB', 25000, 1);
+insert into shipping_provider (id, created_at, description, name, price, shipping_method_id) values (20010, '2020-12-11 16:10:49', 'VN Express', 'VNE', 30000, 1);
+insert into shipping_provider (id, created_at, description, name, price, shipping_method_id) values (30010, '2020-12-11 16:11:49', 'Viettel Post', 'VTP', 25000, 1);
+insert into shipping_provider (id, created_at, description, name, price, shipping_method_id) values (40010, '2020-12-11 16:12:49', 'Giao hang tiet kiem', 'GHTK', 25000 , 1) ;
+insert into shipping_provider (id, created_at, description, name, price, shipping_method_id) values (50010, '2020-12-11 16:13:49', 'Ninja van', 'NJV', 27000, 1);
 
 
 
