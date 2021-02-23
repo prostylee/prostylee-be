3cf78a191739a5a4335f2c139f07539091b9a2ff
Lê Quốc Nhâm <lqnham@gmail.com>
2021-02-23 12:49:34 +0700

    FIx sql problem

--- src/main/resources/db/migration/V1_0_18__Alter_Order.sql
+++ src/main/resources/db/migration/V1_0_18__Alter_Order.sql
@@ -78,5 +78,5 @@ ALTER TABLE "order" ADD COLUMN total_money double precision;
 ALTER TABLE "order" ADD COLUMN shipping_address_id bigint;
 ALTER TABLE "order" ADD COLUMN shipping_provider_id bigint;
 ALTER TABLE "order" RENAME COLUMN payment_type TO payment_type_id;
-ALTER TABLE "order" ALTER COLUMN buyer_id TYPE bigint;
-ALTER TABLE "order" ALTER COLUMN payment_type_id TYPE bigint;
+ALTER TABLE "order" ALTER COLUMN buyer_id TYPE bigint USING (buyer_id::integer);
+ALTER TABLE "order" ALTER COLUMN buyer_id TYPE bigint USING (buyer_id::integer);
