455a2f241de5ea5cb3818e68e206de4b5162e606
Phan Thanh Giang <ptgiang56it@gmail.com>
2021-03-21 13:33:31 +0700

    BE-353: API find stores nearby

--- src/main/java/vn/prostylee/core/dto/filter/BaseFilter.java
+++ src/main/java/vn/prostylee/core/dto/filter/BaseFilter.java
@@ -3,9 +3,12 @@ package vn.prostylee.core.dto.filter;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.validation.Valid;
+
 /**
  * The base class for search with keyword, paging and sorting
  */
+@Valid
 @Data
 @EqualsAndHashCode(callSuper = true)
 public abstract class BaseFilter extends PagingAndSortingParam implements SearchingDefinitions {
--- src/main/java/vn/prostylee/store/dto/filter/MostActiveStoreFilter.java
+++ src/main/java/vn/prostylee/store/dto/filter/MostActiveStoreFilter.java
@@ -3,6 +3,7 @@ package vn.prostylee.store.dto.filter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import vn.prostylee.product.constant.PagingConstant;
 
 @Data
 public class MostActiveStoreFilter {
@@ -11,7 +12,6 @@ public class MostActiveStoreFilter {
      * The default number of days that will be count for considering as most active stores.
      */
     public static final int DEFAULT_TIME_RANGE_IN_DAYS = 30;
-    public static final int DEFAULT_NUMBER_OF_PRODUCT_IN_EACH_STORE = 10;
 
     @Schema(name = "limit", example = "20", description = "Number of records to be received.")
     @Range(min = 1, max = 100)
@@ -19,7 +19,7 @@ public class MostActiveStoreFilter {
 
     @Schema(name = "numberOfProducts", example = "10", description = "Number of products in each store to be received.")
     @Range(min = 1, max = 100)
-    private int numberOfProducts = DEFAULT_NUMBER_OF_PRODUCT_IN_EACH_STORE;
+    private int numberOfProducts = PagingConstant.DEFAULT_NUMBER_OF_PRODUCT_IN_EACH_STORE;
 
     @Schema(name = "timeRangeInDays", example = "30", description = "The number of days that will be count for considering as most active stores.")
     @Range(min = 1, max = 365)
