8a6fe5e86875ee720bf265be52afb87547a0bdf8
Phan Thanh Giang <ptgiang56it@gmail.com>
2021-03-28 01:26:28 +0700

    BE-225: API get suggestion products

---
+++ src/main/java/vn/prostylee/product/converter/ProductConverter.java
@@ -0,0 +1,84 @@
+package vn.prostylee.product.converter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.stereotype.Component;
+import vn.prostylee.auth.service.UserService;
+import vn.prostylee.core.exception.ResourceNotFoundException;
+import vn.prostylee.core.utils.BeanUtil;
+import vn.prostylee.location.dto.response.LocationResponse;
+import vn.prostylee.location.service.LocationService;
+import vn.prostylee.media.constant.ImageSize;
+import vn.prostylee.media.service.FileUploadService;
+import vn.prostylee.product.dto.response.ProductOwnerResponse;
+import vn.prostylee.product.dto.response.ProductResponse;
+import vn.prostylee.product.entity.Product;
+import vn.prostylee.product.entity.ProductImage;
+import vn.prostylee.store.service.StoreService;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ProductConverter {
+
+    private final LocationService locationService;
+    private final FileUploadService fileUploadService;
+    private final UserService userService;
+    private final StoreService storeService;
+
+    public ProductResponse toResponse(Product product) {
+        ProductResponse productResponse = BeanUtil.copyProperties(product, ProductResponse.class);
+        productResponse.setImageUrls(buildImageUrls(product.getProductImages()));
+        productResponse.setLocation(buildLocation(productResponse.getLocationId()));
+        productResponse.setIsAdvertising(false); // TODO Will be implemented after Ads feature completed: https://prostylee.atlassian.net/browse/BE-127
+        productResponse.setProductOwnerResponse(buildProductOwner(product));
+        return productResponse;
+    }
+
+    private List<String> buildImageUrls(Set<ProductImage> productImages) {
+        List<Long> attachmentIds = productImages.stream()
+                .map(ProductImage::getAttachmentId)
+                .collect(Collectors.toList());
+        if (CollectionUtils.isNotEmpty(attachmentIds)) {
+            try {
+                return fileUploadService.getImageUrls(attachmentIds, ImageSize.EXTRA_SMALL.getWidth(), ImageSize.EXTRA_SMALL.getHeight());
+            } catch (ResourceNotFoundException e) {
+                log.debug("Could not build image Urls from attachmentIds={}", attachmentIds, e);
+            }
+        }
+        return Collections.emptyList();
+    }
+
+    private LocationResponse buildLocation(Long locationId) {
+        return Optional.ofNullable(locationId)
+                .flatMap(locationService::fetchById)
+                .orElse(null);
+    }
+
+    private ProductOwnerResponse buildProductOwner(Product product) {
+        final ProductOwnerResponse[] productOwnerResponse = new ProductOwnerResponse[1];
+        if (product.getStoreId() != null) {
+            storeService.fetchById(product.getStoreId()).ifPresent(store ->
+                    productOwnerResponse[0] = ProductOwnerResponse.builder()
+                            .id(store.getId())
+                            .name(store.getName())
+                            .logoUrl(store.getLogoUrl())
+                            .build());
+        } else {
+            userService.fetchById(product.getCreatedBy()).ifPresent(user ->
+                    productOwnerResponse[0] = ProductOwnerResponse.builder()
+                            .id(user.getId())
+                            .name(user.getFullName())
+                            .logoUrl(user.getAvatar())
+                            .build());
+        }
+        return productOwnerResponse[0];
+    }
+}
