e0b9a6e40d1281b31476988c319e33b755f11682
datnguyen <datjp89@gmail.com>
2021-04-17 05:02:18 +0700

    developing

--- src/main/java/vn/prostylee/store/service/StoreService.java
+++ src/main/java/vn/prostylee/store/service/StoreService.java
@@ -1,6 +1,7 @@
 package vn.prostylee.store.service;
 
 import org.springframework.data.domain.Page;
+import vn.prostylee.core.dto.filter.BaseFilter;
 import vn.prostylee.core.service.CrudService;
 import vn.prostylee.store.dto.filter.MostActiveStoreFilter;
 import vn.prostylee.store.dto.filter.StoreProductFilter;
@@ -21,4 +22,6 @@ public interface StoreService extends CrudService<StoreRequest, StoreResponse, L
     Page<StoreMiniResponse> getMiniStoreResponse(StoreProductFilter storeProductFilter);
 
     List<Long> getNewStoreIds(NewestStoreRequest request);
+
+    Page<StoreResponse> searchStoresByKeyword(BaseFilter storeFilter);
 }
