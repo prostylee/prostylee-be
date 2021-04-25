606cb60b5c450384837960252c9cd0c0161d3390
Phan Thanh Giang <ptgiang56it@gmail.com>
2021-04-25 21:07:40 +0700

    BE-386: Get list products by recently view

--- src/main/java/vn/prostylee/product/service/impl/ProductServiceImpl.java
+++ src/main/java/vn/prostylee/product/service/impl/ProductServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import vn.prostylee.core.dto.filter.BaseFilter;
 import vn.prostylee.core.exception.ResourceNotFoundException;
+import vn.prostylee.core.provider.AuthenticatedProvider;
 import vn.prostylee.core.specs.BaseFilterSpecs;
 import vn.prostylee.core.utils.BeanUtil;
 import vn.prostylee.location.dto.request.LocationRequest;
@@ -18,6 +19,7 @@ import vn.prostylee.location.service.LocationService;
 import vn.prostylee.product.constant.ProductStatus;
 import vn.prostylee.product.converter.ProductConverter;
 import vn.prostylee.product.dto.filter.ProductFilter;
+import vn.prostylee.product.dto.filter.RecentViewProductFilter;
 import vn.prostylee.product.dto.request.ProductPriceRequest;
 import vn.prostylee.product.dto.request.ProductRequest;
 import vn.prostylee.product.dto.response.ProductResponse;
@@ -25,10 +27,14 @@ import vn.prostylee.product.entity.*;
 import vn.prostylee.product.repository.ProductRepository;
 import vn.prostylee.product.service.*;
 import vn.prostylee.product.specification.ProductSpecificationBuilder;
+import vn.prostylee.useractivity.constant.TrackingType;
+import vn.prostylee.useractivity.dto.filter.UserTrackingFilter;
+import vn.prostylee.useractivity.service.UserTrackingService;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -43,6 +49,8 @@ public class ProductServiceImpl implements ProductService {
     private final ProductPriceService productPriceService;
     private final ProductConverter productConverter;
     private final ProductSpecificationBuilder searchableBuilder;
+    private final UserTrackingService userTrackingService;
+    private final AuthenticatedProvider authenticatedProvider;
 
     @Override
     public Page<ProductResponse> findAll(BaseFilter baseFilter) {
@@ -88,6 +96,22 @@ public class ProductServiceImpl implements ProductService {
                 .orElseThrow(() -> new ResourceNotFoundException("Product is not found with id [" + id + "]"));
     }
 
+    @Override
+    public List<ProductResponse> getRecentViewProducts(RecentViewProductFilter recentViewProductFilter) {
+        UserTrackingFilter userTrackingFilter = UserTrackingFilter.builder()
+                .userId(authenticatedProvider.getUserIdValue())
+                .trackingType(TrackingType.PRODUCT)
+                .build();
+        userTrackingFilter.setLimit(recentViewProductFilter.getLimit());
+        userTrackingFilter.setPage(recentViewProductFilter.getPage());
+
+        List<Long> productIds = userTrackingService.getLastVisitedIdsBy(userTrackingFilter);
+
+        return productRepository.findProductsByIds(productIds)
+                .stream()
+                .map(productConverter::toResponse)
+                .collect(Collectors.toList());
+    }
 
     @Override
     public ProductResponse findById(Long id) {
--- src/main/java/vn/prostylee/useractivity/service/UserTrackingService.java
+++ src/main/java/vn/prostylee/useractivity/service/UserTrackingService.java
@@ -1,10 +1,15 @@
 package vn.prostylee.useractivity.service;
 
+import vn.prostylee.useractivity.dto.filter.UserTrackingFilter;
 import vn.prostylee.useractivity.dto.request.UserTrackingRequest;
 import vn.prostylee.useractivity.dto.response.UserTrackingResponse;
+
+import java.util.List;
 
 public interface UserTrackingService {
 
     UserTrackingResponse storeTracking(UserTrackingRequest request);
+
+    List<Long> getLastVisitedIdsBy(UserTrackingFilter userTrackingFilter);
 
 }
