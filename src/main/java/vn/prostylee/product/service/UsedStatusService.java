a69adaad22af8510fa354bb2d553d0abae02405e
Phan Thanh Giang <ptgiang56it@gmail.com>
2021-05-02 12:05:19 +0700

    BE-421: Remove used status because we can add it as an attribute

--- src/main/java/vn/prostylee/product/service/UsedStatusService.java
+++
@@ -1,9 +0,0 @@
-package vn.prostylee.product.service;
-
-import vn.prostylee.product.dto.response.UsedStatusResponse;
-
-import java.util.List;
-
-public interface UsedStatusService {
-    List<UsedStatusResponse> getUsedStatuses();
-}
