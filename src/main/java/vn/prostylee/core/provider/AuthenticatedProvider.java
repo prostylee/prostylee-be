973ad73eab6e7a23714aa0582c26575403a0db09
Phan Thanh Giang <ptgiang56it@gmail.com>
2021-06-06 22:34:25 +0700

    BE-485: fix get user id from aws token

--- src/main/java/vn/prostylee/core/provider/AuthenticatedProvider.java
+++ src/main/java/vn/prostylee/core/provider/AuthenticatedProvider.java
@@ -1,23 +1,16 @@
 package vn.prostylee.core.provider;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import vn.prostylee.auth.dto.response.UserCredential;
-import vn.prostylee.auth.entity.User;
-import vn.prostylee.auth.repository.UserRepository;
 
 import java.util.Optional;
 
 @Slf4j
-@RequiredArgsConstructor
 @Component
 public class AuthenticatedProvider {
-
-    private final UserRepository userRepository;
 
     public Optional<Long> getUserId() {
         return getUser().map(UserCredential::getId);
@@ -33,16 +26,6 @@ public class AuthenticatedProvider {
             return Optional.empty();
         }
         UserCredential userCredential = (UserCredential) authentication.getPrincipal();
-        if (userCredential != null && (userCredential.getId() == null || userCredential.getId() <= 0)) {
-            findUserIdBySub(userCredential.getSub()).ifPresent(userCredential::setId);
-        }
         return Optional.ofNullable(userCredential);
-    }
-
-    private Optional<Long> findUserIdBySub(String sub) {
-        if (StringUtils.isNotBlank(sub)) {
-            return userRepository.findActivatedUserBySub(sub).map(User::getId);
-        }
-        return Optional.empty();
     }
 }
