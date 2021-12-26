a097da02740e31a6f28b2baf594acce8d9ff7203
Phan Thanh Giang <ptgiang56it@gmail.com>
2021-12-26 17:03:57 +0700

    Fix API user statistic

--- src/main/java/vn/prostylee/useractivity/dto/filter/UserFollowerFilter.java
+++ src/main/java/vn/prostylee/useractivity/dto/filter/UserFollowerFilter.java
@@ -3,6 +3,8 @@ package vn.prostylee.useractivity.dto.filter;
 import lombok.*;
 import vn.prostylee.core.constant.TargetType;
 
+import java.util.List;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 @Builder
@@ -10,7 +12,15 @@ import vn.prostylee.core.constant.TargetType;
 @AllArgsConstructor
 public class UserFollowerFilter extends UserFollowerPageable {
 
+    /**
+     * deprecated Using targetTypes
+     */
+    @Deprecated
     private TargetType targetType;
+
+    private List<TargetType> targetTypes;
+
     private Long targetId;
+
     private Long userId;
 }
