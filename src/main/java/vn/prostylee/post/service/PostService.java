8caaa8606c34a0dedaed86bcd0b5fc99e0990ab3
Phan Thanh Giang <ptgiang56it@gmail.com>
2021-12-26 17:03:57 +0700

    Fix API user statistic

--- src/main/java/vn/prostylee/post/service/PostService.java
+++ src/main/java/vn/prostylee/post/service/PostService.java
@@ -8,5 +8,7 @@ import vn.prostylee.post.dto.response.PostResponse;
 
 public interface PostService extends CrudService<PostRequest, PostResponse, Long> {
 
+    long countTotalPostByUser(Long userId);
+
     Page<PostResponse> getNewFeeds(PostFilter postFilter);
 }
